d45473c11f7c98ad625108a14d065b2fdc69229e
ruveydakisla <ruveydakisla34@gmail.com>
2023-09-23 14:15:54 +0300

    memo-card added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MemberShipCard from './components/MemberShipCard';
 import Rewalds from './Pages/Rewalds';
 import MemoryNFT from './Pages/MemoryNFT';
 import Membership from './Pages/Membership';
+import MemoryCard from './components/MemoryCard';
 export default function App() {
   return (
     <div>
@@ -23,8 +24,10 @@ export default function App() {
               <Route path="membership" element={<Membership />} />
               <Route path="memorynft" element={<MemoryNFT />} />
               <Route path="rewalds" element={<Rewalds />} />
+              
             </Route>
             <Route path="/Card" element={<Card />} />
+            <Route path="memocard" element={<MemoryCard />} />
           </Routes>
         </div>
       </Router>
---
+++ src/components/MemoryCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function MemoryCard() {
+  return (
+    <div>
+      <div className="memo-card d-flex justify-content-center">
+        <div className='d-flex justify-content-center rounded-circle'>
+          <img
+            class="rounded-circle"
+            alt="avatar1"
+            src="https://picsum.photos/id/14/200/200"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MemoryCard
